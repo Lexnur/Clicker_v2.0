441fb67fecf98b50d9637c9cf0f86b0cee4a5dc6
DelayVeshi <derevchuk.s@gmail.com>
2024-01-11 13:45:23 +0300

    Кликер Мышки + задержка после выполнения

--- venv/Click.py
+++ venv/Click.py
@@ -17,15 +17,22 @@ def clicker():
 
 keyboard.add_hotkey('Ctrl + Q', clicker) # Комбинации клавиш для активации 'кликера'
 
+push = int(input('Введи кол-во кликов:\n'))
+pause = float(input('Введи интервал между кликами в интервале от 0.05 до 1 сек.\n'))
+stop = int(input('Введи время для остановки после нажатий: ... сек.\n'))
+time.sleep(1)
+print('Press the keys: "Ctrl + Q"')
+
+
 i = 0
 while True:
-    if i < 990:
+    if i < push:
         i += 1
         if isClicking:
             mouse.double_click(button='left') # Выбор кнопки мышки
-            time.sleep(0.05) # Задержка после каждого нажатия
+            time.sleep(pause) # Задержка после каждого нажатия
     else:
-        time.sleep(600) # Для передышки после определенного количество нажатий
+        time.sleep(stop) # Для передышки после определенного количество нажатий
         i = 0
 
 
